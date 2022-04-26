c1fcbf5ef5d7151dca118b0dfb948e1952820932
rosenrose <42435748+rosenrose@users.noreply.github.com>
2022-04-26 23:09:10 +0900

    d

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const express = require("express");
 const axios = require("axios").default;
 const PORT = process.env.PORT || 3000;
 
+axios.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    return Promise.resolve(error);
+  }
+);
+
 const app = express();
 app.get("/", (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
   const url = decodeURIComponent(req.query.url);
-  axios(encodeURI(url), { responseType: "arraybuffer" }).then((response) => {
-    res.send(response.data);
+  axios(encodeURI(url)).then((response) => {
+    res.send(response);
   });
 });
 
