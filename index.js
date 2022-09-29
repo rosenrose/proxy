24b4e11c3616f2ef21c15de0b14dd4ffb2e15d8c
rosenrose <crazytempler@gmail.com>
2022-09-29 13:58:27 +0900

    responseType

--- index.js
+++ index.js
@@ -18,20 +18,19 @@ exports.app = (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
 
   const [path, search] = req.url.split("?");
+  console.log(path, search, req.query);
 
-  if (path.endsWith("/")) {
-    res.send("Test");
-  } else if (path.endsWith("/data")) {
-    const url = decodeURIComponent(req.query.url);
-
-    axios(encodeURI(url)).then((response) => {
+  if (path.endsWith("/data")) {
+    axios(req.query.url, { responseType: "arraybuffer" }).then((response) => {
+      res.header("Content-Type", response.headers["content-type"]);
+      // res.header("Content-Length", response.headers["content-length"]);
       res.send(response.data);
     });
   } else if (path.endsWith("/status")) {
-    const url = decodeURIComponent(req.query.url);
-
-    axios(encodeURI(url)).then((response) => {
+    axios(req.query.url, { responseType: "arraybuffer" }).then((response) => {
       res.send(response.status.toString());
     });
+  } else if (path.endsWith("/")) {
+    res.send("Test");
   }
 };
